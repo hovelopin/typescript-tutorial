1a651f29cc98550a01f9286a50e1ddbd491c8182
hovelopin <kimhojin3714@naver.com>
2022-07-08 00:03:03 +0900

    3장 객체와 타입 ( 기본 )

---
+++ ch03/src/Destruction.ts
@@ -0,0 +1,12 @@
+import { IPerson } from "./IPerson";
+
+const person: IPerson = {
+  name: "HP",
+  age: 27
+}
+console.log(person);
+
+const {name , age} = person;
+
+console.log(name , age);
+
---
+++ ch03/src/TypeConversion.ts
@@ -0,0 +1,2 @@
+const person: object = {name: "HP" , age:27}
+console.log(person.name);
